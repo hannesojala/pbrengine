4b241742aed89a90db75e50681fab0eed3599390
Hannes Ojala <hannesojala123@gmail.com>
2021-02-03 20:37:15 -0800

    waow

--- bin/frag.glsl
+++ bin/frag.glsl
@@ -1,11 +1,12 @@
 #version 460 core
 
-in vec2 o_uvCoords;
+in vec2 uvCoords;
+in vec3 normal;
 
 out vec4 FragColor;
 
 uniform sampler2D u_texture;
   
 void main() {
-    FragColor = texture(u_texture, o_uvCoords);
+    FragColor = texture(u_texture, uvCoords);
 }
--- bin/vert.glsl
+++ bin/vert.glsl
@@ -1,13 +1,16 @@
 #version 460 core
 
 in vec3 a_position;
+in vec3 a_normal;
 in vec2 a_uvCoords;
 
-out vec2 o_uvCoords;
+out vec2 uvCoords;
+out vec3 normal;
   
 uniform mat4 u_mvp;
 
 void main() {
-    o_uvCoords = a_uvCoords;
+    uvCoords = a_uvCoords;
+    normal = a_normal;
     gl_Position = u_mvp * vec4(a_position, 1.0);
 }
